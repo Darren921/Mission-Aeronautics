b39a45b270b99885830f00adac9fb2df77f1dbe4
Darren921 <darren.chung@ontariotechu.net>
2024-03-22 22:38:50 -0400

    consistent volume throughout scenes

--- Assets/Script/UI/Volume.cs
+++ Assets/Script/UI/Volume.cs
@@ -6,17 +6,58 @@ using UnityEngine.UI;
 public class Volume : MonoBehaviour
 {
     public int volumeAmount = 4;
-
+    public float currentVolume;
     public Image volumeImage;
 
     public Sprite[] volumeSprites;
 
     public AudioSource volumeThing;
 
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetFloat("Volume", 0.75f);
+            PlayerPrefs.Save();
+        }
+        volumeThing.volume = PlayerPrefs.GetFloat("Volume", 0.75f);
+        PlayerPrefs.Save();
+
+        print(volumeThing.volume);
+
+    }
+
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetFloat("Volume", volumeThing.volume);
+        PlayerPrefs.Save();
+        print(currentVolume);
+    }
     private void Update()
     {
-        volumeImage.sprite = volumeSprites[volumeAmount];
-        volumeThing.volume = (float)volumeAmount / 4;
+        if (volumeSprites != null && volumeSprites.Length > 0)
+        {
+            UpdateVol();
+        }
+      
+    }
+
+    public void UpdateVol()
+    {
+        if(volumeSprites != null && volumeSprites.Length > 0 ) 
+        {
+            volumeImage.sprite = volumeSprites[volumeAmount];
+            volumeThing.volume = (float)volumeAmount / 4;
+            PlayerPrefs.SetFloat("Volume", volumeThing.volume);
+            print(PlayerPrefs.GetFloat("Volume"));
+            PlayerPrefs.Save();
+
+
+        }
+
+
+
     }
 
     public void VolumeUp()
